3cbbe059e43e3e35d9161dccac1b000e822c220b
adarshb07 <adarshbhardwaj285@gmail.com>
2025-09-10 21:13:52 +0530

    Learn the use of Use Effect

--- src/App.js
+++ src/App.js
@@ -1,48 +1,6 @@
 import { useEffect, useState } from "react";
 import StarRating from "./StarRating";
 
-const tempMovieData = [
-	{
-		imdbID: "tt1375666",
-		Title: "Inception",
-		Year: "2010",
-		Poster: "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-	},
-	{
-		imdbID: "tt0133093",
-		Title: "The Matrix",
-		Year: "1999",
-		Poster: "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
-	},
-	{
-		imdbID: "tt6751668",
-		Title: "Parasite",
-		Year: "2019",
-		Poster: "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
-	},
-];
-
-const tempWatchedData = [
-	{
-		imdbID: "tt1375666",
-		Title: "Inception",
-		Year: "2010",
-		Poster: "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-		runtime: 148,
-		imdbRating: 8.8,
-		userRating: 10,
-	},
-	{
-		imdbID: "tt0088763",
-		Title: "Back to the Future",
-		Year: "1985",
-		Poster: "https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
-		runtime: 116,
-		imdbRating: 8.5,
-		userRating: 9,
-	},
-];
-
 const average = (arr) =>
 	arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
@@ -53,7 +11,7 @@ export default function App() {
 	const [watched, setWatched] = useState([]);
 	const [isLoading, setIsLoading] = useState(false);
 	const [error, setError] = useState("");
-	const [query, setQuery] = useState("Harry Potter");
+	const [query, setQuery] = useState("");
 	const [selectedId, setSelectedId] = useState(null);
 
 	function handleSelectMovie(id) {
@@ -78,12 +36,15 @@ export default function App() {
 
 	useEffect(
 		function () {
+			const controller = new AbortController();
+
 			async function fetchMovies() {
 				try {
 					setIsLoading(true);
 					setError("");
 					const res = await fetch(
-						`http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+						`http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+						{ signal: controller.signal }
 					);
 					if (!res.ok)
 						throw new Error(
@@ -94,8 +55,9 @@ export default function App() {
 						throw new Error("Movie not found");
 					setMovies(data.Search);
 				} catch (err) {
-					console.error(err.message);
-					setError(err.message);
+					if (err.name !== "AbortError") {
+						setError(err.message);
+					}
 				} finally {
 					setIsLoading(false);
 				}
@@ -106,7 +68,13 @@ export default function App() {
 				setError("");
 				return;
 			}
+
+			handleCloseMovie();
 			fetchMovies();
+
+			return function () {
+				controller.abort();
+			};
 		},
 		[query]
 	);
@@ -288,6 +256,23 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
 		onCloseMovie();
 	}
 
+	useEffect(
+		function () {
+			function callback(e) {
+				if (e.code === "Escape") {
+					onCloseMovie();
+				}
+			}
+
+			document.addEventListener("keydown", callback);
+
+			return function () {
+				document.removeEventListener("keydown", callback);
+			};
+		},
+		[onCloseMovie]
+	);
+
 	useEffect(() => {
 		async function getMovieDetails() {
 			setIsLoading(true);
@@ -300,6 +285,18 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
 		}
 		getMovieDetails();
 	}, [selectedId]);
+
+	useEffect(
+		function () {
+			if (!title) return;
+			document.title = `Movie | ${title}`;
+
+			return function () {
+				document.title = "usePopcorn";
+			};
+		},
+		[title]
+	);
 
 	return (
 		<div className="details">
@@ -395,7 +392,7 @@ function WatchedMoviesList({ watched, onDeleteWatched }) {
 		<ul className="list">
 			{watched.map((movie) => (
 				<WatchedMovie
-					key={movie.imdbID}
+					key={Date.now()}
 					movie={movie}
 					onDeleteWatched={onDeleteWatched}
 				/>
